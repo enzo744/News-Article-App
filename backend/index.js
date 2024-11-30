1dc2a6493c0c0825a753d5daf934abe0123ce20d
enzo744 <vnalbone0@gmail.com>
2024-11-30 20:10:50 +0100

    Connected to MongDB

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).
+then(() => {
+  console.log("Il database news_portal_db è stato connesso");
+}).catch((err) => {
+  console.log(err);
+});
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(3005, () => {
   console.log("Server is running on port 3005");
