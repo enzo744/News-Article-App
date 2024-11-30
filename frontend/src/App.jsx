6001f4abea7b5ef025b6181d20a870782d540de0
enzo744 <vnalbone0@gmail.com>
2024-11-30 13:12:35 +0100

    Add pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,27 @@
 // import React from 'react'
 
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignInForm from "./auth/forms/SignInForm";
+import SignUpForm from "./auth/forms/SignUpForm";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import NewsAticles from "./pages/NewsAticles";
+
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-      <Button className="bg-red-500" variant="ghost">
-        Click me
-      </Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/news" element={<NewsAticles />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ frontend/src/pages/NewsAticles.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const NewsAticles = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewsAticles
