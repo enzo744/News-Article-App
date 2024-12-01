9e0d82c8247f507bca620706d947f5f90a83522a
enzo744 <vnalbone0@gmail.com>
2024-12-01 13:22:07 +0100

    Add error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -12,7 +13,7 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All fields are required" });
+    return next(errorHandler(400, "Compilare tutti i campi richiesti!"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -27,6 +28,7 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.json( "Signup created successfully" );
   } catch (error) {
-    res.status(500).json({ message: "Error creating user" });
+    // res.status(500).json({ message: "Error creating user" });
+    next(error);
   }
 };
